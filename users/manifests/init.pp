d69c36d9d0cdac7c8540c0fe3d53915f0f464c4d
root <root@ip-172-31-9-184.us-west-2.compute.internal>
2017-10-12 05:16:15 +0000

    User_init.pp_wilma_ssh

--- users/manifests/init.pp
+++ users/manifests/init.pp
@@ -1,34 +1,41 @@
 class user {
 
-  user { 'becca' :
-    ensure     => present,
-    uid        => '10018289',
-    home       => '/home/becca',
-    managehome => true,
-    password   => '!!',
-    groups     => ['sysadmin', 'cars'],
-    shell      => '/bin/bash',
-  }
+	user { 'becca' :
+		ensure     => present,
+		uid        => '10018289',
+		home       => '/home/becca',
+		managehome => true,
+		password   => '!!',
+		groups     => ['sysadmin', 'cars'],
+		shell      => '/bin/bash',
+	}
 
-  user { 'fred' :
-    ensure     => present,
-    uid        => '10028289',
-    home       => '/home/fred',
-    managehome => true,
-    password   => '!!',
-    groups     => ['trucks', 'cars', 'wheel'],
-    shell      => '/bin/csh',
-  }
+	user { 'fred' :
+		ensure     => present,
+		uid        => '10028289',
+		home       => '/home/fred',
+		managehome => true,
+		password   => '!!',
+		groups     => ['trucks', 'cars', 'wheel'],
+		shell      => '/bin/csh',
+	}
 
-  user { 'wilma' :
-    ensure         => present,
-    uid            => '10038289',
-    home           => '/home/wilma',
-    managehome     => true, 
-    password       => '!!',
-    groups         => ['trucks', 'cars', 'ambulances'],
-    shell          => '/bin/bash',
-  }
+	user { 'wilma' :
+		ensure         => present,
+		uid            => '10038289',
+		home           => '/home/wilma',
+		managehome     => true, 
+		password       => '!!',
+		groups         => ['trucks', 'cars', 'ambulances'],
+		shell          => '/bin/bash',
+	}
+
+        -> ssh_authorized_key { 'wilma_ssh' :
+                ensure => present,
+                user   => 'wilma',
+                type   => 'ssh-rsa',
+                key    => 'WilmansKey', #to be generated using ssh-keygen
+        }
 }
 
 #creation of users
