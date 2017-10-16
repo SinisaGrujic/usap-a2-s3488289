f438d4b7aa5483defc366bc0ad0e9a697e23bf95
root <root@ip-172-31-9-184.us-west-2.compute.internal>
2017-10-16 07:03:23 +0000

    Package_init.pp_alteration_&_addition_mysql

--- services/manifests/init.pp
+++ services/manifests/init.pp
@@ -27,4 +27,8 @@ class services {
                 require => Package['openssh-server'],
         }
 
+        service { 'mysql' :
+                ensure  => running,
+                enable  => true,
+        }
 }
